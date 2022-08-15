5b468b2b637872c36aee9246f1f61ac495fb8673
Huy <xinhtupeanh@gmail.com>
2022-08-16 01:47:46 +0700

    fixbug cors login

--- src/server.js
+++ src/server.js
@@ -3,9 +3,36 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
 import connectDB from "./config/connectDB";
+// import cors from "cors";
+
 require("dotenv").config();
 
 let app = express();
+// app.use(cors({ origin: true }));
+// Cors handle exactly
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", process.env.URL_REACT); //Exactly link reactjs
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 //config app
 
