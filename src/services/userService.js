d5c237f913913cc0cf40195ccd3ed624b77ab4eb
Huy <xinhtupeanh@gmail.com>
2022-08-19 18:26:05 +0700

    fix add new user and errMessage

--- src/services/userService.js
+++ src/services/userService.js
@@ -114,23 +114,24 @@ let createNewUser = (data) => {
       if (check === true) {
         resolve({
           errCode: 1,
-          message:
+          errMessage:
             "Email address has already been used. Please try another email",
+        });
+      } else {
+        let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+        //creat sequelize user
+        await db.User.create({
+          email: data.email,
+          password: hashPasswordFromBcrypt,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          address: data.address,
+          phonenumber: data.phonenumber,
+          gender: data.gender === "1" ? true : false,
+          roleId: data.roleId,
         });
       }
 
-      let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-      //creat sequelize user
-      await db.User.create({
-        email: data.email,
-        password: hashPasswordFromBcrypt,
-        firstName: data.firstName,
-        lastName: data.lastName,
-        address: data.address,
-        phonenumber: data.phonenumber,
-        gender: data.gender === "1" ? true : false,
-        roleId: data.roleId,
-      });
       resolve({
         errCode: 0,
         message: "OK",
