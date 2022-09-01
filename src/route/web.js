974e48a4b74c7b06496804cf2e5985f317765173
Huy <xinhtupeanh@gmail.com>
2022-09-01 23:05:49 +0700

    36 Update for React Load Data, create api of the Doctors( and fix keyMap

--- src/route/web.js
+++ src/route/web.js
@@ -1,6 +1,7 @@
 import express from "express";
 import homeController from "../controllers/homeController";
 import userController from "../controllers/userController";
+import doctorController from "../controllers/doctorController";
 
 let router = express.Router();
 
@@ -13,6 +14,7 @@ let initWebRoutes = (app) => {
   router.get("/delete-crud", homeController.deleteCRUD);
   router.get("/api/get-all-users", userController.handleGetAllUsers);
   router.get("/api/allcode", userController.getAllCode);
+  router.get("/api/top-doctor-home", doctorController.getTopDoctorHome);
 
   router.post("/post-crud", homeController.postCRUD);
   router.post("/put-crud", homeController.putCRUD);
