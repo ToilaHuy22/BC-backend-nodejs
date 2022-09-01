bca1a2ff894e9914aea6149cca21d39b377a3aa4
Huy <xinhtupeanh@gmail.com>
2022-09-01 13:35:32 +0700

    34 Update for Write and Read Image, Sequelize, MySQL, BLOB, xampp, my.ini

--- src/services/userService.js
+++ src/services/userService.js
@@ -130,6 +130,7 @@ let createNewUser = (data) => {
           gender: data.gender,
           roleId: data.roleId,
           positionId: data.positionId,
+          image: data.avatar,
         });
       }
 
@@ -188,6 +189,7 @@ let updateUserData = (data) => {
         user.positionId = data.positionId;
         user.gender = data.gender;
         user.phonenumber = data.phonenumber;
+        user.image = data.avatar;
 
         await user.save();
 
