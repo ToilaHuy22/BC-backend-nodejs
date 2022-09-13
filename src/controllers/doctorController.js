73ae383c31f442feb959caed99d52cf198360380
Huy <xinhtupeanh@gmail.com>
2022-09-13 14:39:35 +0700

    57 Update, Create Api get Profile Doctor for component ProfileDoctor

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -93,6 +93,19 @@ let getExtraInforDoctorById = async (req, res) => {
   }
 };
 
+let getProfileDoctorById = async (req, res) => {
+  try {
+    let infor = await doctorService.getProfileDoctorById(req.query.doctorId);
+    return res.status(200).json(infor);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: 'Error from server',
+    });
+  }
+};
+
 module.exports = {
   getTopDoctorHome: getTopDoctorHome,
   getAllDoctors: getAllDoctors,
@@ -101,4 +114,5 @@ module.exports = {
   bulkCreateSchedule: bulkCreateSchedule,
   getScheduleByDate: getScheduleByDate,
   getExtraInforDoctorById: getExtraInforDoctorById,
+  getProfileDoctorById: getProfileDoctorById,
 };
